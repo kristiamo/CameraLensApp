612d4cd32d32ee6c0a00a15822de2ad178e8317c
kristiamo <kristiamo@hotmail.com>
2026-08-08 02:13:30 +1000

    added plugin package.json back, oops

--- src/composables/useCamera.js
+++ src/composables/useCamera.js
@@ -1,10 +1,8 @@
 ﻿import { ref } from 'vue';
-import { registerPlugin, Capacitor } from '@capacitor/core';
+import { registerPlugin } from '@capacitor/core';
 
-const CameraLensPlugin = registerPlugin('CameraLensPlugin',
-    !Capacitor.isNativePlatform() ?
-        { web: () => import('../mocks/custom-plugins.js').then(m => m.MockCameraLensPlugin) }
-        : {}
+const CameraLensPlugin = registerPlugin('CameraLensPlugin'
+    , { web: () => import('../mocks/custom-plugins.js').then(m => m.MockCameraLensPlugin) }
 );
 
 export function useCamera() {
